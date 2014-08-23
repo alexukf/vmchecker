5800d722d5f2542c290ecb7037a6a1e15aeec58c
Adrian Bacircea <adrian.bacircea@gmail.com>
2014-08-23 17:44:34 +0300

    Fix the JSON encoding

--- vmchecker/database/util.py
+++ vmchecker/database/util.py
@@ -1,16 +1,20 @@
 # -*- coding: utf-8 -*-
 
-from .mixins import ApiResource
 from flask.json import JSONEncoder
+from datetime import datetime
+
+from .mixins import ApiResource
 
 class ApiJSONEncoder(JSONEncoder):
     """ Custom JSON Encoder to handle API Resources """
 
     def default(self, obj):
         if isinstance(obj, ApiResource):
-            print obj
             json = {}
             for key in obj.__class__.get_json_keys():
-                json[key] = getattr(obj, key, '')
+                json[key] = self.default(getattr(obj, key, ''))
             return json
+        if isinstance(obj, datetime):
+            return obj.strftime('%H:%M %d/%m/%Y')
+
         return JSONEncoder.default(obj)
