530578d2de40fe3136f8b0569063c1394931c97c
Adrian Bacircea <adrian.bacircea@gmail.com>
2014-08-30 01:53:44 +0300

    Add a debug mode for serve.py

--- serve.py
+++ serve.py
@@ -3,6 +3,20 @@
 
 from gevent.wsgi import WSGIServer
 from vmchecker.webservice import application
+from werkzeug.serving import run_with_reloader
+from werkzeug.debug import DebuggedApplication
 
-http_server = WSGIServer(('', 5000), application)
-http_server.serve_forever()
+def run_gevent_server():
+    http_server = WSGIServer(('', 5000), DebuggedApplication(application))
+    http_server.serve_forever()
+
+def run_flask_server():
+    application.debug = True
+    application.run(host='0.0.0.0')
+
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) == 2 and sys.argv[1] == 'flask':
+        run_flask_server()
+    else:
+        run_gevent_server()
